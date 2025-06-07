3c0ccc4e3089a03816846cc4c2aa98163d7b4ada
yihuang <yi.codeplayer@gmail.com>
2025-06-07 17:30:22 +0800

    use flake

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
+    utils.url = "github:numtide/flake-utils";
+    foundry.url = "github:shazow/foundry.nix/stable"; # Use stable branch for permanent releases
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+      foundry,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ foundry.overlay ];
+        };
+      in
+      {
+
+        devShell =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              anvil
+              python312Full
+              uv
+            ];
+          };
+      }
+    );
+}
