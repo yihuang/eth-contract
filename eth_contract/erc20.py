095ac2a2322da1efea24fd44d846ebc8db5d8a50
yihuang <yi.codeplayer@gmail.com>
2025-06-13 10:58:43 +0800

    cleanup

--- eth_contract/erc20.py
+++ eth_contract/erc20.py
@@ -4,4 +4,4 @@ from pathlib import Path
 from .contract import Contract
 
 ERC20_ABI = json.loads(Path(__file__).parent.joinpath("abis/erc20.json").read_text())
-ERC20 = Contract(ERC20_ABI["abi"])
+ERC20 = Contract(ERC20_ABI)
--- tests/test_erc20.py
+++ tests/test_erc20.py
@@ -1,7 +1,14 @@
+import json
+from pathlib import Path
+
 import pytest
-from eth_contract.create2 import CREATE2_FACTORY, create2_deploy
-from eth_contract.erc20 import ERC20, ERC20_ABI
+from eth_contract.create2 import create2_deploy
+from eth_contract.erc20 import ERC20
 from eth_contract.utils import get_initcode
+
+MockERC20_ARTIFACT = json.loads(
+    Path(__file__).parent.joinpath("contracts/MockERC20.json").read_text()
+)
 
 
 @pytest.mark.asyncio
@@ -16,22 +23,12 @@ async def test_erc20_live(w3):
 
 @pytest.mark.asyncio
 async def test_create2_deploy(w3):
-    accts = await w3.eth.accounts
-    acct = w3.eth.account.create()
-    fund = 10**18
-    tx_hash = await w3.eth.send_transaction(
-        {"from": accts[0], "to": acct.address, "value": fund}
-    )
-    res = await w3.eth.wait_for_transaction_receipt(tx_hash)
-    assert res["status"] == 1
-    balance = await w3.eth.get_balance(acct.address)
-    assert balance == fund
-    price = await w3.eth.gas_price
-    tx = {
-        "from": acct.address,
-        "gas": "0x2625a0",
-        "gasPrice": hex(price),
-    }
-    initcode = get_initcode(ERC20_ABI)
-    res = await create2_deploy(w3, initcode, acct, 0, CREATE2_FACTORY, extra=tx)
-    assert res == "0x06aF35e31BC77ADcE5a4865B1Ec04F8E7c97E3eD"
+    owner = (await w3.eth.accounts)[0]
+    initcode = get_initcode(MockERC20_ARTIFACT, "TEST", "TEST", 18)
+    token = await create2_deploy(w3, initcode, extra={"from": owner})
+    assert token == "0x3C565f3a7520637276e13bc2697B604aA8842131"
+
+    tx = {"to": token, "from": owner}
+    assert await ERC20.fns.balanceOf(owner).call(w3, tx=tx) == 0
+    await ERC20.fns.mint(owner, 1000).transact(w3, tx=tx)
+    assert await ERC20.fns.balanceOf(owner).call(w3, tx=tx) == 1000
