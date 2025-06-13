cc1d50257b18554c837f59ecb90443b3e07256f7
yihuang <yi.codeplayer@gmail.com>
2025-06-13 14:05:20 +0800

    vendor multicall3 pre-signed tx

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,48 +1,74 @@
+import asyncio
 import socket
-import subprocess
 import time
-from typing import Generator
+from typing import AsyncGenerator
 
-import pytest
+import pytest_asyncio
+from eth_contract.multicall3 import (DEPLOY_SIGNED_TX, DEPLOYER_ADDRESS,
+                                     MULTICALL3_ADDRESS)
+from eth_contract.utils import send_transaction
 from web3 import AsyncHTTPProvider, AsyncWeb3
+from web3.types import Wei
 
 PORT = 9545
 
 
-def wait_port(port: int, host: str = "localhost", timeout: float = 10.0) -> None:
-    """Wait until a port is open on the given host."""
-    start_time = time.perf_counter()
-    while True:
+async def await_port(port: int, retries: int = 100, host="127.0.0.1") -> None:
+    """Check if a port is open and available for connection."""
+    for i in range(retries):
         try:
-            with socket.create_connection((host, port), timeout=1):
-                return
-        except (socket.timeout, ConnectionRefusedError):
-            if time.perf_counter() - start_time >= timeout:
-                raise TimeoutError(
-                    f"Timed out waiting for port {port} on {host} after "
-                    f"{timeout} seconds"
-                )
-            time.sleep(0.1)
+            reader, writer = await asyncio.open_connection(host, port)
+            writer.close()
+            await writer.wait_closed()
+            return True
+        except (ConnectionRefusedError, asyncio.TimeoutError):
+            await asyncio.sleep(0.1)
+    raise asyncio.TimeoutError(
+        f"Port {port} did not become available after {retries} retries."
+    )
 
 
-@pytest.fixture(scope="session")
-def w3() -> Generator[AsyncWeb3, None, None]:
-    anvil = subprocess.Popen(
-        [
-            "anvil",
-            "--fork-url",
-            "https://eth-mainnet.public.blastapi.io",
-            "--fork-block-number",
-            "18000000",
-            "--port",
-            str(PORT),
-        ]
+async def ensure_multicall3_deployed(w3: AsyncWeb3):
+    if await w3.eth.get_code(MULTICALL3_ADDRESS):
+        # already deployed
+        print("multicall3 already deployed at", MULTICALL3_ADDRESS)
+        return
+    amount = Wei(10**17)
+    if await w3.eth.get_balance(DEPLOYER_ADDRESS) < amount:
+        funder = (await w3.eth.accounts)[0]
+        await send_transaction(
+            w3,
+            tx={
+                "from": funder,
+                "to": DEPLOYER_ADDRESS,
+                "value": amount,
+            },
+        )
+    txhash = await w3.eth.send_raw_transaction(DEPLOY_SIGNED_TX)
+    receipt = await w3.eth.wait_for_transaction_receipt(txhash)
+    assert receipt["status"] == 1, "Multicall3 deployment failed"
+    assert receipt["contractAddress"] == MULTICALL3_ADDRESS
+    print("multicall3 deployed at", MULTICALL3_ADDRESS)
+
+
+@pytest_asyncio.fixture(scope="session")
+async def w3() -> AsyncGenerator[AsyncWeb3, None]:
+    proc = await asyncio.create_subprocess_exec(
+        "anvil",
+        "-q",
+        "--fork-url",
+        "https://eth-mainnet.public.blastapi.io",
+        "--fork-block-number",
+        "18000000",
+        "--port",
+        str(PORT),
     )
 
     try:
-        wait_port(PORT)
-    except TimeoutError:
-        anvil.terminate()
-        raise
-    yield AsyncWeb3(AsyncHTTPProvider(f"http://localhost:{PORT}"))
-    anvil.terminate()
+        await await_port(PORT)
+        w3 = AsyncWeb3(AsyncHTTPProvider(f"http://localhost:{PORT}"))
+        await ensure_multicall3_deployed(w3)
+        yield w3
+    finally:
+        proc.terminate()
+        await proc.wait()
