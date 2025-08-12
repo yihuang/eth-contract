b9f7ad1878d7d8130af44beef4870dd65faa9426
yihuang <yi.codeplayer@gmail.com>
2025-08-12 23:55:03 +0800

    Problem: erc4337 entrypoint contracts are not included

---
+++ eth_contract/entrypoint.py
@@ -0,0 +1,45 @@
+"""
+ERC-4337 EntryPoint
+
+https://github.com/eth-infinitism/account-abstraction/
+"""
+
+import json
+from pathlib import Path
+
+from eth_utils import to_checksum_address
+from hexbytes import HexBytes
+
+from .contract import Contract
+
+ENTRYPOINT08_ARTIFACT = json.loads(
+    Path(__file__).parent.joinpath("deployments/EntryPoint08.json").read_text()
+)
+ENTRYPOINT08_ABI = ENTRYPOINT08_ARTIFACT["abi"]
+ENTRYPOINT08 = Contract(ENTRYPOINT08_ABI)
+ENTRYPOINT08_SALT = HexBytes(
+    "0x0a59dbff790c23c976a548690c27297883cc66b4c67024f9117b0238995e35e9"
+)
+ENTRYPOINT08_ADDRESS = to_checksum_address(ENTRYPOINT08_ARTIFACT["address"])
+
+ENTRYPOINT07_ARTIFACT = json.loads(
+    Path(__file__).parent.joinpath("deployments/EntryPoint07.json").read_text()
+)
+ENTRYPOINT07_ABI = ENTRYPOINT07_ARTIFACT["abi"]
+ENTRYPOINT07 = Contract(ENTRYPOINT07_ABI)
+ENTRYPOINT07_SALT = HexBytes(
+    "0x90d8084deab30c2a37c45e8d47f49f2f7965183cb6990a98943ef94940681de3"
+)
+ENTRYPOINT07_ADDRESS = to_checksum_address(ENTRYPOINT07_ARTIFACT["address"])
+
+
+if __name__ == "__main__":
+    from .create2 import create2_address
+    from .utils import get_initcode
+
+    assert ENTRYPOINT08_ADDRESS == create2_address(
+        get_initcode(ENTRYPOINT08_ARTIFACT), ENTRYPOINT08_SALT
+    )
+    assert ENTRYPOINT07_ADDRESS == create2_address(
+        get_initcode(ENTRYPOINT07_ARTIFACT), ENTRYPOINT07_SALT
+    )
