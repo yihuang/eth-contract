6a3b9d641c9b72eb7ec81c551269f6c6ea37162e
yihuang <yi.codeplayer@gmail.com>
2025-10-07 17:37:08 +0800

    cleanup

--- eth_contract/human.py
+++ eth_contract/human.py
@@ -108,10 +108,41 @@ $""",
     re.VERBOSE,
 )
 
-INTEGER_REGEX = re.compile(
-    r"^u?int(8|16|24|32|40|48|56|64|72|80|88|96|104|112|120|128|136|144|152|160|168"
-    r"|176|184|192|200|208|216|224|232|240|248|256)$"
-)
+INTEGER_SIZES = [
+    8,
+    16,
+    24,
+    32,
+    40,
+    48,
+    56,
+    64,
+    72,
+    80,
+    88,
+    96,
+    104,
+    112,
+    120,
+    128,
+    136,
+    144,
+    152,
+    160,
+    168,
+    176,
+    184,
+    192,
+    200,
+    208,
+    216,
+    224,
+    232,
+    240,
+    248,
+    256,
+]
+INTEGER_REGEX = re.compile(rf"^u?int({'|'.join(map(str, INTEGER_SIZES))})$")
 BYTES_REGEX = re.compile(r"^bytes([1-9]|[12][0-9]|3[0-2])$")
 
 # Modifier sets
@@ -124,7 +155,13 @@ parameter_cache: dict[str, ABIComponentIndexed] = {}
 
 # struct signature regex
 STRUCT_SIGNATURE_REGEX = re.compile(
-    r"^struct (?P<name>[a-zA-Z$_][a-zA-Z0-9$_]*)\s*\{\s*(?P<properties>.*)\s*\}$"
+    rf"""^struct\s+
+(?P<name>{IDENTIFIER})\s*
+\{{\s*
+  (?P<properties>.*)
+\s*\}}
+$""",
+    re.VERBOSE,
 )
 
 
